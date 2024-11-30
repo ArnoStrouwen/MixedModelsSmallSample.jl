198b467e69fb4b695858208cce931da65acee0c1
ArnoStrouwen <arno.strouwen@telenet.be>
2024-11-30 23:32:46 +0100

    try adjusted df

--- prototype.jl
+++ prototype.jl
@@ -47,3 +47,41 @@ end
 
 varcovar_adjusted = m.vcov + 2*m.vcov*factor*m.vcov
 adjusted_error = sqrt.([varcovar_adjusted[i,i] for i in 1:size(m.vcov,1)])
+
+c = 1
+p = length(adjusted_error)
+C = zeros(p,c)
+C[2,1] = 1
+M = C*inv(C'*varcovar*C)*C'
+A1 = 0.0
+for i in eachindex(dVinv_dσ)
+    for j in eachindex(dVinv_dσ)
+        Pi = X'*dVinv_dσ[i]*X
+        Pj = X'*dVinv_dσ[j]*X
+        Wij = W[i,j]
+        A1 += Wij*tr(M*varcovar*Pi*varcovar)*tr(M*varcovar*Pj*varcovar)
+    end
+end
+
+A2 = 0.0
+for i in eachindex(dVinv_dσ)
+    for j in eachindex(dVinv_dσ)
+        Pi = X'*dVinv_dσ[i]*X
+        Pj = X'*dVinv_dσ[j]*X
+        Wij = W[i,j]
+        A2 += Wij*tr(M*varcovar*Pi*varcovar*M*varcovar*Pj*varcovar)
+    end
+end
+
+B = (A1 + 6A2)/(2c)
+g = ((c+1)A1 - (c+4)A2)/((c+2)A2)
+c1 = g/(3c + 2(1-g))
+c2 = (c-g)/(3c+2(1-g))
+c3 = (c+2-g)/(3c+2(1-g))
+Estar = inv(1-A2/c)
+Vstar = (2/c)*(1+c1*B)/((1-c2*B)^2*(1-c3*B))
+#Estar = 1 + A2/c
+#Vstar = 2(1+B)/c
+ρ = Vstar/(2*Estar^2)
+m = 4 + (c+2)/(c*ρ-1)
+λ = m/(Estar*(m-2))

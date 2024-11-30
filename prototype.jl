1605e74f6fa02f1f076c549c92a5e721a1d6bfe0
ArnoStrouwen <arno.strouwen@telenet.be>
2024-11-30 16:41:00 +0100

    unadjusted varcovar

--- prototype.jl
+++ prototype.jl
@@ -17,4 +17,12 @@ one_minus_one_coding!(df[!,:MC])
 one_minus_one_coding!(df[!,:SS])
 
 fm = @formula(LEI~ 1 + (1|Day) + FR + MC + SS + FR&MC + FR&SS + MC&SS + FR&FR + MC&MC + SS&SS)
-m = fit(MixedModel, fm, df)
+m = fit(MixedModel, fm, df; REML = true)
+
+σ_eps = m.sigma
+σ_gam = m.sigmas[1][1]
+Z = m.reterms[1]'
+V = diagm(fill(σ_eps^2, size(df,1))) + σ_gam^2*Z'*Z
+X = m.X
+varcovar = inv(X'*inv(V)*X)
+m.vcov

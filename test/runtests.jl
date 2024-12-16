94caade5d999f36eaa2759d9963b4669a4d32ad0
ArnoStrouwen <arno.strouwen@telenet.be>
2024-12-16 21:17:10 +0100

    add split plot experiment

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,4 +15,7 @@ using SafeTestsets
     @safetestset "Scientific tests" begin
         include("kenward_roger.jl")
     end
+    @safetestset "split plot experiment" begin
+        include("split plot experiment.jl")
+    end
 end

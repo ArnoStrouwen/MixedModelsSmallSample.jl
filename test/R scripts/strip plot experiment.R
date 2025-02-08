899b30b2718e05ad30dc8373163e0570e467b014
ArnoStrouwen <arno.strouwen@telenet.be>
2025-02-08 16:35:45 +0100

    add strip plot test

---
+++ test/R scripts/strip plot experiment.R
@@ -0,0 +1,11 @@
+library(lmerTest)
+df <- read.csv("Data battery cell Chapter 11.csv")
+fm <- lmer(Y~ 1 + X1 + X2 + X3 + X4 + X5 + X6 +
+              X1*X2 + X1*X3 + X1*X4 + X1*X5 + X1*X6 +
+              X2*X3 + X2*X4 + X2*X5 + X2*X6 +
+              X3*X4 + X3*X5 + X3*X6 +
+              X4*X5 + X4*X6 +
+              X5*X6 +
+              (1 | Whole.Plots) + (1 | Subplots), data=df)
+test = summary(fm, ddf = "Kenward-Roger")
+write.csv(test[10], "Results battery cell lmertest.csv")

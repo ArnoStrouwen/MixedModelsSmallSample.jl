b02af920fbd9461878e02437a1069c79537998e2
ArnoStrouwen <arno.strouwen@telenet.be>
2025-03-09 14:19:36 +0100

    handle correlation between random effects

--- test/random slope.jl
+++ test/random slope.jl
@@ -41,3 +41,67 @@ res = DataFrame(CSV.File("Results sleep study lmertest.csv"))
     rtol=1e-8,
 )
 @test isapprox(res[!, "coefficients.df"], kr.v, atol=1e-10, rtol=1e-9)
+
+fm = @formula(reaction ~ 1 + days + (1 + days | subj))
+m = fit(MixedModel, fm, df; REML=true)
+kr = adjust_KR(m; FIM_σ²=:observed_SAS_MATCHING)
+
+res = DataFrame(CSV.File("Results sleep study corr sas kr.csv"))
+@test isapprox(res[!, "Estimate"], kr.m.β, atol=1e-10, rtol=1e-10)
+@test isapprox(res[!, "StdErr"], sqrt.(diag(kr.varcovar_adjusted)), atol=1e-10, rtol=1e-5)
+@test isapprox(res[!, "DF"], kr.v, atol=1e-10, rtol=1e-4)
+
+sw = adjust_SW(m; FIM_σ²=:observed_SAS_MATCHING)
+
+res = DataFrame(CSV.File("Results sleep study corr sas sw.csv"))
+@test isapprox(res[!, "Estimate"], sw.m.β, atol=1e-10, rtol=1e-10)
+@test isapprox(res[!, "StdErr"], sw.m.stderror, atol=1e-5, rtol=1e-5)
+@test isapprox(res[!, "DF"], sw.v, atol=1e-10, rtol=1e-4)
+
+fm = @formula(reaction ~ 1 + days + days^2 + zerocorr(1 + days + days^2 | subj))
+m = fit(MixedModel, fm, df; REML=true)
+kr = adjust_KR(m; FIM_σ²=:observed_SAS_MATCHING)
+
+res = DataFrame(CSV.File("Results sleep study quadratic sas kr.csv"))
+@test isapprox(res[!, "Estimate"], kr.m.β, atol=1e-10, rtol=1e-10)
+@test isapprox(res[!, "StdErr"], sqrt.(diag(kr.varcovar_adjusted)), atol=1e-10, rtol=1e-5)
+@test isapprox(res[!, "DF"], kr.v, atol=1e-10, rtol=1e-4)
+
+sw = adjust_SW(m; FIM_σ²=:observed_SAS_MATCHING)
+
+res = DataFrame(CSV.File("Results sleep study quadratic sas sw.csv"))
+@test isapprox(res[!, "Estimate"], sw.m.β, atol=1e-10, rtol=1e-10)
+@test isapprox(res[!, "StdErr"], sw.m.stderror, atol=1e-10, rtol=1e-5)
+@test isapprox(res[!, "DF"], sw.v, atol=1e-10, rtol=1e-4)
+
+fm = @formula(reaction ~ 1 + days + days^2 + (1 + days + days^2 | subj))
+m = fit(MixedModel, fm, df; REML=true)
+kr = adjust_KR(m; FIM_σ²=:observed_SAS_MATCHING)
+
+res = DataFrame(CSV.File("Results sleep study corr quadratic sas kr.csv"))
+@test isapprox(res[!, "Estimate"], kr.m.β, atol=1e-10, rtol=1e-10)
+@test isapprox(res[!, "StdErr"], sqrt.(diag(kr.varcovar_adjusted)), atol=1e-10, rtol=1e-4)
+@test isapprox(res[!, "DF"], kr.v, atol=1e-10, rtol=1e-4)
+
+sw = adjust_SW(m; FIM_σ²=:observed_SAS_MATCHING)
+
+res = DataFrame(CSV.File("Results sleep study corr quadratic sas sw.csv"))
+@test isapprox(res[!, "Estimate"], sw.m.β, atol=1e-10, rtol=1e-10)
+@test isapprox(res[!, "StdErr"], sw.m.stderror, atol=1e-10, rtol=1e-4)
+@test isapprox(res[!, "DF"], sw.v, atol=1e-10, rtol=1e-4)
+
+fm = @formula(reaction ~ 1 + days + days^2 + (1 | subj) + (days + days^2 | subj))
+m = fit(MixedModel, fm, df; REML=true)
+kr = adjust_KR(m; FIM_σ²=:observed_SAS_MATCHING)
+
+res = DataFrame(CSV.File("Results sleep study some corr quadratic sas kr.csv"))
+@test isapprox(res[!, "Estimate"], kr.m.β, atol=1e-10, rtol=1e-10)
+@test isapprox(res[!, "StdErr"], sqrt.(diag(kr.varcovar_adjusted)), atol=1e-10, rtol=1e-4)
+@test isapprox(res[!, "DF"], kr.v, atol=1e-10, rtol=1.0e-3)
+
+sw = adjust_SW(m; FIM_σ²=:observed_SAS_MATCHING)
+
+res = DataFrame(CSV.File("Results sleep study some corr quadratic sas sw.csv"))
+@test isapprox(res[!, "Estimate"], sw.m.β, atol=1e-10, rtol=1e-10)
+@test isapprox(res[!, "StdErr"], sw.m.stderror, atol=1e-5, rtol=1e-4)
+@test isapprox(res[!, "DF"], sw.v, atol=1e-10, rtol=1.0e-3)

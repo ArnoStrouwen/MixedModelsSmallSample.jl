d6eb09330a075121c25dce32ce1e57fbbbc62f94
ArnoStrouwen <arno.strouwen@telenet.be>
2026-01-04 13:17:21 +0100

    deal with AVX differences

--- test/random slope.jl
+++ test/random slope.jl
@@ -64,14 +64,14 @@ kr = adjust_KR(m; FIM_σ²=:observed_SAS_MATCHING)
 
 res = DataFrame(CSV.File("results/Results sleep study quadratic sas kr.csv"))
 @test isapprox(res[!, "Estimate"], kr.m.β, atol=1e-10, rtol=1e-10)
-@test isapprox(res[!, "StdErr"], sqrt.(diag(kr.varcovar_adjusted)), atol=1e-10, rtol=1e-5)
+@test isapprox(res[!, "StdErr"], sqrt.(diag(kr.varcovar_adjusted)), atol=1e-10, rtol=1e-4)
 @test isapprox(res[!, "DF"], kr.v, atol=1e-10, rtol=1e-4)
 
 sw = adjust_SW(m; FIM_σ²=:observed_SAS_MATCHING)
 
 res = DataFrame(CSV.File("results/Results sleep study quadratic sas sw.csv"))
 @test isapprox(res[!, "Estimate"], sw.m.β, atol=1e-10, rtol=1e-10)
-@test isapprox(res[!, "StdErr"], sw.m.stderror, atol=1e-10, rtol=1e-5)
+@test isapprox(res[!, "StdErr"], sw.m.stderror, atol=1e-10, rtol=1e-4)
 @test isapprox(res[!, "DF"], sw.v, atol=1e-10, rtol=1e-4)
 
 fm = @formula(reaction ~ 1 + days + days^2 + (1 + days + days^2 | subj))
